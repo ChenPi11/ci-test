31087f765b716e093e0dd8db53b48c59aec338cf
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2025-01-07 20:55:41 +0800

    test

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 FILE=/opt/homebrew/var/lib/tor/hidden_service/hostname
 while [ ! -f "$FILE" ]; do
     echo "Waiting for $FILE ..."
@@ -36,6 +36,11 @@ echo =====================================
 echo =====================================
 echo =====================================
 
-brew services logs tor > /tmp/service1.log &
-brew services logs code-server > /tmp/service2.log &
-tail -f /tmp/service1.log /tmp/service2.log | tee /dev/stdout
+#brew services logs tor > /tmp/service1.log &
+#brew services logs code-server > /tmp/service2.log &
+#tail -f /tmp/service1.log /tmp/service2.log | tee /dev/stdout
+while true
+do
+ls /opt/homebrew/var/log/
+sleep 10
+done
